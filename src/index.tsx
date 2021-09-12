f84753b47f0ff557c589ef95090c3b4803a66b0b
Ivan Moskalev <ivan.moskalev@gmail.com>
2021-09-12 17:13:49 +0300

    chore: remove JS code as this library is native-only

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NativeModules } from 'react-native';
-
-type ReactNativeCompressedJsbundleType = {
-  multiply(a: number, b: number): Promise<number>;
-};
-
-const { ReactNativeCompressedJsbundle } = NativeModules;
-
-export default ReactNativeCompressedJsbundle as ReactNativeCompressedJsbundleType;
